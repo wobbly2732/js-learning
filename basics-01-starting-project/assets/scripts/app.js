cce67a14eb25829d438b82e8b34ba81d20a7512d
wobbly2732 <tim.chapman2732@gmail.com>
2021-05-26 15:51:29 +0100

    enabled all calcs

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,14 +1,50 @@
 //variables
+
 const defaultResult = 0;
 let currentResult = defaultResult;
-let calculationDescription = '';
 
 //functions
-function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, calculationDescription);
+
+function getUserNumberInput() {
+    return parseInt(userInput.value)
 }
 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multipy() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
 
 //main
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multipy);
+divideBtn.addEventListener('click', divide);
